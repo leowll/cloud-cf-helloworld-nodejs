cf6c14cea7c88fddaee32fd61778dfc1bbe44921
Wang <liang.liang.wang@sap.com>
2019-12-10 15:12:13 +0900

    add timeout config

--- uiveri5/conf.js
+++ uiveri5/conf.js
@@ -2,4 +2,9 @@ exports.config = {
   profile: 'integration',
 
   baseUrl: 'https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/master-detail/webapp/test/mockServer.html',
+  timeouts: {
+  　　getPageTimeout: '30000',
+  　　allScriptsTimeout: '11000',
+  　　defaultTimeoutInterval: '30000'
+　　}
 };
